507d514ff251b95fe6ec5912b187c66d142c2aa8
Mr.Igor <iv_mirsoft@ukr.net>
2025-06-18 19:49:58 +0300

    icon++

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ const MSG_ERROR_DIGITS = "Sorry, there is error in your request. Please, try to 
 const searchForm = document.querySelector(".form");
 const searchField = document.querySelector(".input_delay");
 
-const iconPath = 'error.svg';
+const iconPath = './img/error.svg';
 
 let page = 1;
 let perPage = 10;
